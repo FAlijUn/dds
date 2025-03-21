168711cfb18f95bf6b9ac3bbcc29470aa17e731a
lijun.fang <lijun.fang@ubtrobot.com>
2025-03-21 19:10:04 +0800

    feat: add plugin use

---
+++ ros2_plugin_use/src/main.cpp
@@ -0,0 +1,20 @@
+#include <rclcpp/rclcpp.hpp>
+#include <pluginlib/class_loader.hpp>
+#include "plugin_test_impl.h"
+
+int main(int argc, char** argv) {
+  rclcpp::init(argc, argv);
+  auto node = std::make_shared<rclcpp::Node>("plugin_test_node");
+
+  pluginlib::ClassLoader<LiJun::PluginTest> loader("ros2_plugin_test", "LiJun::PluginTest");
+
+  try {
+    std::shared_ptr<LiJun::PluginTest> plugin = loader.createSharedInstance("LiJun::PluginTestImpl");
+    plugin->DoSomething();
+  } catch (const pluginlib::LibraryLoadException& e) {
+    RCLCPP_ERROR(node->get_logger(), "Failed to load plugin: %s", e.what());
+  }
+
+  rclcpp::shutdown();
+  return 0;
+}
